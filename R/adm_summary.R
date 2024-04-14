1ac3e924db9b002a3051d3aefb23dd78d8a2c23c
Shijia1997 <shijiazhang199710@gmail.com>
2024-04-14 13:30:10 -0400

    update the plot.files dealing with categorical time

--- R/adm_summary.R
+++ R/adm_summary.R
@@ -316,6 +316,8 @@ plot.files <- function(path, FILE_pattern = "\\.xlsx$|\\.xls$|\\.csv$", dict_src
   
   return(combined_data)
   
+}
+  
   
 #   # p <- plot_ly(data = combined_data, x = ~DATE, y = ~ID, type = 'scatter', mode = 'markers',
 #   #              hoverinfo = 'text', # Display the hover text when hovering
